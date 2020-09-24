d77d4204b0716b771af9aab96d2c97add3b01141
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-09-23 17:08:38 -0700

    add 3 buttons to change the background and fails at getting them working"

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ myButton.addEventListener('click', () => {
     const userInput = myInput.value;
 
     myName.textContent = userInput;
+
+    myInput.value = ''; 
 });
 
 const button = document.querySelector("#flip-button");
@@ -14,3 +16,26 @@ button.addEventListener("click", e => {
 
 document.querySelector(".sticker").classList.toggle("flip-scale-down-diag-2")
 })
+
+// I'm at a lost for what I am doing wrong here.
+
+const newColor1 = document.querySelector("main");
+color1.addEventListener('click', () => {
+  console.log('blue');
+  const color1 = document.body; 
+  color1-button.classList.toggle('color1');
+});
+
+const newColor2 = document.querySelector("main");
+color2.addEventListener('click', () => {
+  console.log('blue');
+  const color2 = document.body; 
+  color2-button.classList.toggle('color2');
+});
+
+const newColor3 = document.querySelector("main");
+color3.addEventListener('click', () => {
+  console.log('blue');
+  const color3 = document.body; 
+  color3-button.classList.toggle('color3');
+});
